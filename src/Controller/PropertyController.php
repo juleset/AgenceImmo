9446fc6ee6474572368961a52e23655f55a54f1e
juleset <jules.etourneau@hotmail.com>
2021-10-12 21:36:29 +0200

    deux premieres pages

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -5,16 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
 
-class HomeController extends AbstractController
+class PropertyController extends AbstractController
 {
+
     /**
-     * @Route("/" , name="home")
+     * @Route("/biens", name="property.index")
      * @return Response
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('property/index.html.twig', ['current_menu' => 'properties']);
     }
 }
